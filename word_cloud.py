cbd99b29a5fa01c753295962c92d0e9437d2f4f2
Supriya-techie <101581957+Supriya-techie@users.noreply.github.com>
2022-03-18 11:45:58 -0500

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -1,3 +1,35 @@
+# Here are all the installs and imports you will need for your word cloud script and uploader widget
+
+!pip install wordcloud
+!pip install fileupload
+!pip install ipywidgets
+!jupyter nbextension install --py --user fileupload
+!jupyter nbextension enable --py fileupload
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+# This is the uploader widget
+def _upload():
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+_upload()
+
+Hint: Try storing the results of your iteration in a dictionary before passing them into wordcloud via the generate_from_frequencies function.
 def calculate_frequencies(file_contents):
     # Here is a list of punctuations and uninteresting words you can use to process your text
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -11,31 +43,31 @@ def calculate_frequencies(file_contents):
     file_contents_list = file_contents.split()
     new_file_contents_list = []
     new_contents_file = ""
-    
     # LEARNER CODE START HERE
     #removing punctuions
     for text in file_contents:
         if text in punctuations:
-            file_contents = file_contents.replace(text,"")
-            
+            file_contents = file_contents.replace(text,""        
     #removing words with numbers
     for text in file_contents_list:
         if text.isalpha():
-            new_file_contents_list.append(text)
-            
+            new_file_contents_list.append(text)        
     #removing uninteresting words         
-    
     for file_list in new_file_contents_list:
          if file_list not in uninteresting_words:
-                new_contents_file = new_contents_file+" "+file_list
-                
+                new_contents_file = new_contents_file+" "+file_list           
     #count frequencies
     word_list = new_contents_file.split()
     word_frequencies = {}
     for word in word_list:
         word_frequencies[word]=word_list.count(word)    
-    
     #wordcloud
     cloud = wordcloud.WordCloud()
     cloud.generate_from_frequencies(word_frequencies)
     return cloud.to_array()
+     # Display your wordcloud image
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()                                             
